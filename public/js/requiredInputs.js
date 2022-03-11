9bf2fae32e9fb6e111bb0e3d0237ebdd507a91b7
Nuria <nuria.villaronga@xercode.com>
2022-03-11 16:46:49 +0100

    new changes validation.js

---
+++ public/js/requiredInputs.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    /*
+
+    var titleRequired = document.getElementById("title_BE");
+    var titleWithoutPrefixRequired = document.getElementById("titleWP_BE");
+
+    console.log(titleRequired);
+
+    if (titleRequired != null) {
+        titleWithoutPrefixRequired.setAttribute("style", "class: form-control requiredInput;");
+    }
+    */
+    /*
+    if (titleRequired == null) {
+        titleWithoutPrefixRequired.removeAttribute("class");
+        titleWithoutPrefixRequired.setAttribute("style", "class: form-control");
+    }
+    */
+});
--- public/js/validation.js
+++ public/js/validation.js
@@ -1,32 +1,55 @@
+$.validator.setDefaults({
+    debug: true, 
+    errorClass: 'text-danger',
+    highlight: function(element) {
+      $(element)
+        .closest('.requiredInputs')
+        .addBack().css( "border-color", "red" );
+    },
+    unhighlight: function(element) {
+      $(element)
+        .closest('.requiredInput')
+        .addBack().css( "border-color", "rgb(185, 185, 185)" ); //Cambiar al color necesario
+    }
+});
+
+
 $(function() {
-
-    $.validator.setDefaults({
-        debug: true, 
-        errorClass: 'text-danger',
-        highlight: function(element) {
-          $(element)
-            .closest('.requiredInputs')
-            .addBack().css( "border-color", "red" );
-        },
-        unhighlight: function(element) {
-          $(element)
-            .closest('.requiredInput')
-            .addBack().css( "border-color", "rgb(185, 185, 185)" ); //Cambiar al color necesario
-        },
-    });
-
+    /*
     $.validator.addMethod("publishingDateFormat", function( value, element ) {
         return this.optional( element ) || 
             /^(?:(0{2,3}[1-9]|0{1,2}[1-9]\d|0[1-9]\d{2}|[1-9]\d{3})(?:(0[1-9]|1[0-2])(0[1-9]|1\d|2[0-8])|(0[13-9]|1[0-2])(29|30)|(0[13578]|1[02])(31))|(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[13579][26]|[2468][048])00)(0?2)(29))$/.test( value );
     }, "Introduce una fecha válida en formato AAAAMMDD");
+    */
 
-    $.validator.addMethod("editionNumberType", function( value, element ) {
-        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
-    }, "Introduce un número de edición válido");
+    $('#general_information').validate({
+        ignore: '.select2-search__field, input[type=hidden]',
+        rules: {
+            "general_information_form[publishingDate]": {
+                required : true
+            },
+            "general_information_form[editionNumber]": {
+                digits: true
+            },
+            "general_information_form[numberOfPages]": {
+                digits: true
+            }
+        },
+        messages: {
+            "general_information_form[publishingDate]": {
+                required : 'Fecha de publicación es obligatorio'
+            },
+            "general_information_form[numberOfPages]": {
+                digits: 'Introduce un número de páginas válido'
+            },
+            "general_information_form[numberOfPages]": {
+                digits: 'Introduce un número de edición válido'
+            }
+        },
+    });
+});
 
-    $.validator.addMethod("numberPagesType", function( value, element ) {
-        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
-    }, "Introduce un número de páginas válido");
+$(function() {
 
     $.validator.addMethod("isbn13Option", function( value, element ) {
         return this.optional( element ) || 
@@ -53,8 +76,7 @@ $(function() {
                 isbn13Option : true
             },
             "basic_edition_form[EAN]": {
-                required : true,
-                eanOption : true
+                required : true
             },
             "basic_edition_form[ISBN10]": {
                 isbn10Option : true
@@ -84,26 +106,4 @@ $(function() {
             }
         }
     });
-    
-    $("#general_information").validate({
-        ignore: ".select2-search__field",
-        rules: {
-            "general_information_form[publishingDate]": {
-                required : true,
-                publishingDateFormat : true
-            },
-            "general_information_form[editionNumber]": {
-                editionNumberType : true
-            },
-            "general_information_form[numberOfPages]": {
-                numberPagesType : true
-            }
-        },
-        messages: {
-            "general_information_form[publishingDate]": {
-                required : 'Fecha de publicación es obligatorio'
-            }
-        }
-    });
-
 });
