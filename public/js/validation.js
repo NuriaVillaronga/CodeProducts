d7e72a02c90fe6465a4a4cfbc81b34d1a48d377f
Nuria <nuria.villaronga@xercode.com>
2022-03-11 13:27:19 +0100

    changes on validation

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,6 +1,7 @@
 $(function() {
 
     $.validator.setDefaults({
+        debug: true, 
         errorClass: 'text-danger',
         highlight: function(element) {
           $(element)
@@ -13,23 +14,34 @@ $(function() {
             .addBack().css( "border-color", "rgb(185, 185, 185)" ); //Cambiar al color necesario
         },
     });
-      
+
+    $.validator.addMethod("publishingDateFormat", function( value, element ) {
+        return this.optional( element ) || 
+            /^(?:(0{2,3}[1-9]|0{1,2}[1-9]\d|0[1-9]\d{2}|[1-9]\d{3})(?:(0[1-9]|1[0-2])(0[1-9]|1\d|2[0-8])|(0[13-9]|1[0-2])(29|30)|(0[13578]|1[02])(31))|(\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[13579][26]|[2468][048])00)(0?2)(29))$/.test( value );
+    }, "Introduce una fecha válida en formato AAAAMMDD");
+
+    $.validator.addMethod("editionNumberType", function( value, element ) {
+        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
+    }, "Introduce un número de edición válido");
+
+    $.validator.addMethod("numberPagesType", function( value, element ) {
+        return this.optional( element ) || value >= 0 && Number.isInteger(Number(value));
+    }, "Introduce un número de páginas válido");
+
     $.validator.addMethod("isbn13Option", function( value, element ) {
         return this.optional( element ) || 
-            /^(?=[0-9]{13}$)97[89][]?[0-9]{1,5}?[0-9]?[0-9]+[]?$/.test( value );
+        /^(?=[0-9]{13}$)97[89][]?[0-9]{1,5}?[0-9]?[0-9]+[]?$/.test( value );
     }, "ISBN13 must contain <u>13 digits</u> and start with <em>978</em> or <em>979</em>");
-
+        
     $.validator.addMethod("isbn10Option", function( value, element ) {
         return this.optional( element ) || 
-            /^(?=[0-9X]{10}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
+        /^(?=[0-9X]{10}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
     }, "ISBN10 must contain <u>10 digits</u>");
-
+        
     $.validator.addMethod("eanOption", function( value, element ) {
         return this.optional( element ) || 
-            /^(?=[0-9X]{13}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
+        /^(?=[0-9X]{13}$)[0-9]?[0-9]+[]?[0-9X]$/.test( value );
     }, "EAN must contain <u>13 digits</u>");
-
-    //-----------------------------
 
     $("#basic_edition").validate({
         rules: {
@@ -72,4 +84,26 @@ $(function() {
             }
         }
     });
+    
+    $("#general_information").validate({
+        ignore: ".select2-search__field",
+        rules: {
+            "general_information_form[publishingDate]": {
+                required : true,
+                publishingDateFormat : true
+            },
+            "general_information_form[editionNumber]": {
+                editionNumberType : true
+            },
+            "general_information_form[numberOfPages]": {
+                numberPagesType : true
+            }
+        },
+        messages: {
+            "general_information_form[publishingDate]": {
+                required : 'Fecha de publicación es obligatorio'
+            }
+        }
+    });
+
 });
