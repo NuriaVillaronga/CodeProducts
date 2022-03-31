2716f5e30f007bfe8de8a55e739a29bb5d081278
Nuria <nuria.villaronga@xercode.com>
2022-03-31 11:41:36 +0200

    FIX for each tabs contributors (OK)

--- public/js/changeKN_contributor.js
+++ public/js/changeKN_contributor.js
@@ -1,32 +1,50 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    /*
-    var contributorsForm = document.querySelectorAll('.contributors_form_tab_comun');
+    var contributorsTab = document.querySelectorAll('.contributor_own_tab');
 
-    contributorsForm.forEach((contributor) => {
-        console.log(contributor.childNodes);
+    contributorsTab.forEach((tab) => {
+        var tabChildrenNodes = tab.childNodes;
+        tabChildrenNodes.forEach((tabChildren) => { 
+            if(tabChildren.nodeName == "FORM") {
+                
+                var personNameInverted;
+                var personName;
+                var keyNames;
+                var namesBeforeKey; 
+
+                for (let i = 0; i < tabChildren.length; i++) {
+                    console.log(tabChildren[i].id);
+                    if(tabChildren[i].id == "contributor_form_personNameInverted") {
+                        personNameInverted = tabChildren[i];
+                    }
+                    if(tabChildren[i].id == "contributor_form_personName") {
+                        personName = tabChildren[i];
+                    }
+                    if(tabChildren[i].id == "contributor_form_keyNames") {
+                        keyNames = tabChildren[i];
+                    }
+                    if(tabChildren[i].id == "contributor_form_namesBeforeKey") {
+                        namesBeforeKey = tabChildren[i];
+                    }
+                }
+
+                namesBeforeKey.addEventListener('keyup', () => { changeValues.call(this.changeValues); });
+
+                keyNames.addEventListener('keyup', () => { changeValues.call(this.changeValues); });
+
+                function changeValues() {
+                    if(personName.value != null) {
+                        personName.value = namesBeforeKey.value+" "+keyNames.value;
+                    }
+                    if(personNameInverted.value != null) {
+                        personNameInverted.value = keyNames.value+", "+namesBeforeKey.value;
+                    }
+                    if(keyNames.value == null || namesBeforeKey.value == null) {
+                        personNameInverted.value = keyNames.value+namesBeforeKey.value;
+                    }
+                }
+            }
+        });
     });
-    */
-
-    var personNameInverted = document.getElementById('contributor_form_personNameInverted');
-    var personName = document.getElementById('contributor_form_personName');
-    var keyNames = document.getElementById('contributor_form_keyNames');
-    var namesBeforeKey = document.getElementById('contributor_form_namesBeforeKey'); 
-
-    namesBeforeKey.addEventListener('keyup', () => { changeValues.call(this.changeValues); });
-
-    keyNames.addEventListener('keyup', () => { changeValues.call(this.changeValues); });
-
-    function changeValues() {
-        if(personName.value != null) {
-            personName.value = namesBeforeKey.value+" "+keyNames.value;
-        }
-        if(personNameInverted.value != null) {
-            personNameInverted.value = keyNames.value+", "+namesBeforeKey.value;
-        }
-        if(keyNames.value == null || namesBeforeKey.value == null) {
-            personNameInverted.value = keyNames.value+namesBeforeKey.value;
-        }
-    }
     
 });
