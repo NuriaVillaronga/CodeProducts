6c4c8badccd9e69f967f3970c787131823cc32b9
Nuria <nuria.villaronga@xercode.com>
2022-04-04 16:05:24 +0200

    optimize titleBE.js

--- public/js/titleBE.js
+++ public/js/titleBE.js
@@ -7,6 +7,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var title = document.getElementById('basic_edition_form_titleText');
 
 
+    function disable_enable_TTW_TP(option1, option2, title) {
+        if(option1.value != "" && option2.value == "" && title.value == "") {
+            option2.disabled = false;  
+            title.disabled = true;
+        }
+        
+        if(option1.value == "" && option2.value != "" && title.value == "") {
+            option2.disabled = false;
+            title.disabled = true;
+        }
+    
+        if(option1.value == "" && option2.value == "" && title.value == "") {
+            option2.disabled = false;
+            title.disabled = false;
+        }
+    
+        if(option1.value == "" && option2.value == "" && title.value != "") {
+            option2.disabled = true;
+            title.disabled = false; 
+        }
+    }
+
+
     if(title.value == "" && (titlePrefix.value == "" || titleWithoutPrefix == "")) {
         titleWithoutPrefix.disabled = false;
         titlePrefix.disabled = false;
@@ -42,26 +65,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     titleWithoutPrefix.addEventListener('keyup', () => {
         
         titleWithoutPrefix.setAttribute('value', titleWithoutPrefix.value);
-        
-        if(titleWithoutPrefix.value != "" && titlePrefix.value == "" && title.value == "") {
-            titlePrefix.disabled = false; 
-            title.disabled = true;
-        }
-        
-        if(titleWithoutPrefix.value == "" && titlePrefix.value != "" && title.value == "") {
-            titlePrefix.disabled = false;
-            title.disabled = true;
-        }
-    
-        if(titleWithoutPrefix.value == "" && titlePrefix.value == "" && title.value == "") {
-            titlePrefix.disabled = false;
-            title.disabled = false;
-        }
-    
-        if(titleWithoutPrefix.value == "" && titlePrefix.value == "" && title.value != "") {
-            titlePrefix.disabled = true;
-            title.disabled = false; 
-        }
+        disable_enable_TTW_TP.call(this.disable_enable_TTW_TP, titleWithoutPrefix, titlePrefix, title);
     
     });
     
@@ -69,31 +73,46 @@ document.addEventListener("DOMContentLoaded", function (event) {
     titlePrefix.addEventListener('keyup', () => {
         
         titlePrefix.setAttribute('value', titlePrefix.value);
-        
-        if(titlePrefix.value != "" && titleWithoutPrefix.value == "" && title.value == "") {  
-            titleWithoutPrefix.disabled = false;  
-            title.disabled = true; 
-        }
-        
-        if(titlePrefix.value == "" && titleWithoutPrefix.value != "" && title.value == "") {
-            titleWithoutPrefix.disabled = false;
-            title.disabled = true;
-        }
-    
-        if(titlePrefix.value == "" && titleWithoutPrefix.value == "" && title.value == "") {
-            titleWithoutPrefix.disabled = false;
-            title.disabled = false;
-        }
-    
-        if(titlePrefix.value == "" && titleWithoutPrefix.value == "" && title.value != "") {
-            titleWithoutPrefix.disabled = true;
-            title.disabled = false;
-        }
+        disable_enable_TTW_TP.call(this.disable_enable_TTW_TP, titlePrefix, titleWithoutPrefix, title);
     
     });
 
 
     buttonSubmit.addEventListener('click', () => {
+
+        function styling_TWP_TP(option1, option2, errorOption2, styleOption2, title, errorT, styleT) {
+            if(option1.value != "" && option2.value == "" && title.value == "") {    
+                errorOption2.style.display = "inline";
+                errorT.style.display = "none";  
+                styleOption2.setAttribute("style","border-color:red");
+                styleT.setAttribute("style","border-color: rgb(185, 185, 185)");  
+            }
+            
+            if(option1.value == "" && option2.value != "" && title.value == "") {
+                errorOption2.style.display = "inline";
+                errorT.style.display = "none";
+                styleOption2.setAttribute("style","border-color:red");
+                styleT.setAttribute("style","border-color: rgb(185, 185, 185)"); 
+            }
+        
+            if(option1.value == "" && option2.value == "" && title.value == "") {
+                errorOption2.style.display = "inline";
+                errorT.style.display = "inline";
+                styleOption2.setAttribute("style","border-color:red");
+                styleT.setAttribute("style","border-color:red");
+            }
+        
+            if(option1.value == "" && option2.value == "" && title.value != "") {
+                errorOption2.style.display = "none";
+                errorT.style.display = "inline";
+                styleOption2.setAttribute("style","border-color: rgb(185, 185, 185)");
+                styleT.setAttribute("style","border-color:red");
+            }
+
+            if(option1.value == "" && option2.value != "") {
+                styleOption2.setAttribute("style","border-color: rgb(185, 185, 185)");
+            }
+        }
         
         if(titlePrefix.value == "" && title.value == "" && titleWithoutPrefix.value == "") {
 
@@ -122,6 +141,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         
             });
 
+            
             titlePrefix.addEventListener('keyup', () => {
 
                 var errorT = document.getElementById('basic_edition_form_titleText-error');
@@ -130,39 +150,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 var styleT = document.getElementById('basic_edition_form_titleText');
         
                 titlePrefix.setAttribute('value', titlePrefix.value);
-                
-                if(titlePrefix.value != "" && titleWithoutPrefix.value == "" && title.value == "") {  
-                    errorTWP.style.display = "inline";
-                    errorT.style.display = "none";  
-                    styleTWP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color: rgb(185, 185, 185)"); 
-                }
-                
-                if(titlePrefix.value == "" && titleWithoutPrefix.value != "" && title.value == "") {
-                    errorTWP.style.display = "inline";
-                    errorT.style.display = "none";
-                    styleTWP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color: rgb(185, 185, 185)"); 
-                }
-            
-                if(titlePrefix.value == "" && titleWithoutPrefix.value == "" && title.value == "") {
-                    errorTWP.style.display = "inline";
-                    errorT.style.display = "inline";
-                    styleTWP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color:red");
-                }
-            
-                if(titlePrefix.value == "" && titleWithoutPrefix.value == "" && title.value != "") {
-                    errorTWP.style.display = "none";
-                    errorT.style.display = "inline";
-                    styleTWP.setAttribute("style","border-color: rgb(185, 185, 185)");
-                    styleT.setAttribute("style","border-color:red");
-                }
+                styling_TWP_TP.call(this.styling_TWP_TP, titlePrefix, titleWithoutPrefix, errorTWP, styleTWP, title, errorT, styleT);
 
-                if(titlePrefix.value == "" && titleWithoutPrefix.value != "") {
-                    styleTWP.setAttribute("style","border-color: rgb(185, 185, 185)");
-                }
-            
             });
     
     
@@ -174,39 +163,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 var styleT = document.getElementById('basic_edition_form_titleText');
             
                 titleWithoutPrefix.setAttribute('value', titleWithoutPrefix.value);
-                
-                if(titleWithoutPrefix.value != "" && titlePrefix.value == "" && title.value == "") {
-                    errorTP.style.display = "inline";
-                    errorT.style.display = "none";
-                    styleTP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color: rgb(185, 185, 185)");
-                }
-                
-                if(titleWithoutPrefix.value == "" && titlePrefix.value != "" && title.value == "") {
-                    errorTP.style.display = "inline";
-                    errorT.style.display = "none";
-                    styleTP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color: rgb(185, 185, 185)");    
-                }
-            
-                if(titleWithoutPrefix.value == "" && titlePrefix.value == "" && title.value == "") {
-                    errorTP.style.display = "inline";
-                    errorT.style.display = "inline"; 
-                    styleTP.setAttribute("style","border-color:red");
-                    styleT.setAttribute("style","border-color:red");
-                }
-            
-                if(titleWithoutPrefix.value == "" && titlePrefix.value == "" && title.value != "") {
-                    errorTP.style.display = "none"; 
-                    errorT.style.display = "inline";
-                    styleTP.setAttribute("style","border-color: rgb(185, 185, 185)");
-                    styleT.setAttribute("style","border-color:red");
-                }
+                styling_TWP_TP.call(this.styling_TWP_TP, titleWithoutPrefix, titlePrefix, errorTP, styleTP, title, errorT, styleT);
 
-                if(titleWithoutPrefix.value == "" && titlePrefix.value != "") {
-                    styleTP.setAttribute("style","border-color: rgb(185, 185, 185)");
-                }
-            
             });
         }
 
