3a37ccbec1b7461c849945853dfdfdf81a5f51e4
Nuria <nuria.villaronga@xercode.com>
2022-04-04 16:47:18 +0200

    optimze selectRCT_hidde_show.js

--- public/js/selectRCT_hidde_show.js
+++ public/js/selectRCT_hidde_show.js
@@ -7,21 +7,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var suppliersTab = document.querySelectorAll('.supplier_own_tab');
 
     suppliersTab.forEach((tab) => { 
+
         var tabChildrenNodes = tab.childNodes;
+
         tabChildrenNodes.forEach((tabChildren) => { 
+
             if(tabChildren.nodeName == "FORM") {
                 
                 var tabGrandchildrenNodes = tabChildren.childNodes;
-
                 var gicNode;
-
+                var container_2_Node;
+                var container_3_Node;
+                var returnsCodeGrid;
+                var input00;
+                var input01; 
+                var select02;
+                var select03;
+                var select04;
+                var selectRCT;
+                var returnsCode;
+                
                 tabGrandchildrenNodes.forEach((tabGrandchildren)=> {
                     if(tabGrandchildren.id == "container_1") {
                         gicNode = tabGrandchildren.childNodes;
                     }
                 });
-
-                var container_2_Node;
 
                 gicNode.forEach((gicChildren)=> {
                     if(gicChildren.id == "container_2") {
@@ -29,15 +39,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     } 
                 });
 
-                var container_3_Node;
-
                 container_2_Node.forEach((cont2Children)=> {
                     if(cont2Children.id == "container_3") {
                         container_3_Node = cont2Children.childNodes;
                     } 
                 });
-
-                var returnsCodeGrid;
 
                 container_3_Node.forEach((cont3Children)=> {
                     if(cont3Children.id == "returnsCodeGrid") {
@@ -45,14 +51,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     } 
                 });
 
-                var input00;
-                var input01; 
-                var select02;
-                var select03;
-                var select04;
-            
-                var selectRCT;
-                var returnsCode;
                 for (let i = 0; i < tabChildren.length; i++) {
                     if(tabChildren[i].id == "supplier_form_returnsCodeType") {
                         selectRCT = tabChildren[i];
@@ -64,11 +62,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                 optionsRCTselected.call(this.optionsRCTselected, selectRCT.options[selectRCT.selectedIndex].value, returnsCodeGrid);
 
-                selectRCT.addEventListener('change',
-                    () => {
-                            showValuesReturnsCodeChange.call(this.showValuesReturnsCodeChange, selectRCT.options[selectRCT.selectedIndex].value);
-                        }
-                );
+                selectRCT.addEventListener('change', () => {
+                    showValues_on_Change.call(this.showValues_on_Change, selectRCT.options[selectRCT.selectedIndex].value);
+                });
 
                 function optionsRCTselected(selectedOptionRCT, returnsCodeGrid) { 
             
@@ -96,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                             }
             
                             createSelect.call(this.createSelect, returnsCodeGrid, selectID, arrayDataRC);
-                            showValuesReturnsCode.call(this.showValuesReturnsCode, selectedOptionRCT);
+                            showValues_on_Init.call(this.showValues_on_Init, selectedOptionRCT);
                         }
                     };
                 }
@@ -112,38 +108,80 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         select.appendChild(option);
                     }
             
-                    select.setAttribute("class", "form-select");
-                    select.setAttribute("id", selectID);
-                    select.setAttribute("style","display:none"); //cambiar despues a display none
+                    setBasicOptionsNewElements.call(this.setBasicOptionsNewElements, select, selectID, "form-select");
             
                     returnsCodeGrid.appendChild(select);
 
-                    select.addEventListener('change',
-                        () => {
+                    select.addEventListener('change', () => {
                             var selectedOptionRC = select.options[select.selectedIndex].value;
-                            var returnsCode_hidden = returnsCode;
-                            returnsCode_hidden.setAttribute('value', selectedOptionRC);
-                        }
-                    );
+                            var returnsCodeHidden = returnsCode;
+                            returnsCodeHidden.setAttribute('value', selectedOptionRC);
+                    });
                 }
             
                 function createInput(returnsCodeGrid, inputID) {
                     var input = document.createElement("input");
-                    input.setAttribute("class", "form-control");
-                    input.setAttribute("id", inputID); 
-                    input.setAttribute("style","display:none"); //cambiar despues a display none
+                    setBasicOptionsNewElements.call(this.setBasicOptionsNewElements, input, inputID, "form-control");
             
                     returnsCodeGrid.appendChild(input);
 
-                    input.addEventListener('keyup',
-                        () => {
-                            var returnsCode_hidden = returnsCode; 
-                            returnsCode_hidden.setAttribute('value', input.value);
-                        }
-                    );
+                    input.addEventListener('keyup', () => {
+                            var returnsCodeHidden = returnsCode; 
+                            returnsCodeHidden.setAttribute('value', input.value);
+                    });
                 }
 
-                function showValuesReturnsCodeChange(selectedOptionRCT) {
+                function setBasicOptionsNewElements(element, elementID, boots_class) {
+                    element.setAttribute("class", boots_class);
+                    element.setAttribute("id", elementID); 
+                    element.setAttribute("style","display:none"); 
+                }
+
+                function showValues_on_Change(selectedOptionRCT) { 
+                    
+                    setNodeValue.call(this.setNodeValue);
+                    
+                    if(selectedOptionRCT == 00) {
+                        hiddeRestNodes.call(this.hiddeRestNodes, select02, select03, select04, input01, input00); 
+                    }
+                    else if(selectedOptionRCT == 01) {
+                        hiddeRestNodes.call(this.hiddeRestNodes, select02, select03, select04, input00, input01);
+                    }
+                    else if(selectedOptionRCT == 02) {
+                        hiddeRestNodes.call(this.hiddeRestNodes, select03, select04, input00, input01, select02);
+                    }
+                    else if(selectedOptionRCT == 03) {
+                        hiddeRestNodes.call(this.hiddeRestNodes, select02, select04, input00, input01, select03);
+                    }
+                    else if(selectedOptionRCT == 04) {
+                        hiddeRestNodes.call(this.hiddeRestNodes, select02, select03, input00, input01, select04); 
+                    }
+                }
+
+                function showValues_on_Init(selectedOptionRCT) { 
+            
+                    var returnsCodeHidden = returnsCode; 
+
+                    setNodeValue.call(this.setNodeValue);
+                    
+                    if(selectedOptionRCT == 00) {
+                        display_save_SelectedOption.call(this.display_save_SelectedOption, input00, returnsCodeHidden);
+                    }
+                    else if(selectedOptionRCT == 01) {
+                        display_save_SelectedOption.call(this.display_save_SelectedOption, input01, returnsCodeHidden);
+                    }
+                    else if(selectedOptionRCT == 02) {
+                        display_save_SelectedOption.call(this.display_save_SelectedOption, select02, returnsCodeHidden);
+                    }
+                    else if(selectedOptionRCT == 03) {
+                        display_save_SelectedOption.call(this.display_save_SelectedOption, select03, returnsCodeHidden);
+                    }
+                    else if(selectedOptionRCT == 04) {
+                        display_save_SelectedOption.call(this.display_save_SelectedOption, select04, returnsCodeHidden);
+                    }
+                }
+
+                function setNodeValue() {
                     returnsCodeGrid.childNodes.forEach((rcgChild)=> {
                         if(rcgChild.id == "input00") {
                             input00 = rcgChild
@@ -161,110 +199,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
                             select04 = rcgChild
                         }
                     });
-                    
-                    if(selectedOptionRCT == 00) {
-                        hiddeNodes_whileInput.call(this.hiddeNodes_whileInput, select02, select03, select04, input01); 
-                        input00.style.display='block';
-                    }
-                    else if(selectedOptionRCT == 01) {
-                        hiddeNodes_whileInput.call(this.hiddeNodes_whileInput, select02, select03, select04, input00); 
-                        input01.style.display='block';
-                    }
-                    else if(selectedOptionRCT == 02) {
-                        hiddeNodes_whileSelect.call(this.hiddeNodes_whileSelect, select03, select04, input00, input01);
-                        select02.style.display='block';
-                    }
-                    else if(selectedOptionRCT == 03) {
-                        hiddeNodes_whileSelect.call(this.hiddeNodes_whileSelect, select02, select04, input00, input01);
-                        select03.style.display='block';
-                    }
-                    else if(selectedOptionRCT == 04) {
-                        hiddeNodes_whileSelect.call(this.hiddeNodes_whileSelect, select02, select03, input00, input01);
-                        select04.style.display='block'; 
+                }
+
+                function display_save_SelectedOption(node, returnsCodeHidden) {
+                    if(node != null) {
+                        node.style.display='block'; 
+                        node.value = returnsCodeHidden.value;
                     }
                 }
 
-                function showValuesReturnsCode(selectedOptionRCT) {
-                    returnsCodeGrid.childNodes.forEach((rcgChild)=> {
-                        if(rcgChild.id == "input00") {
-                            input00 = rcgChild
-                        }
-                        if(rcgChild.id == "input01") {
-                            input01 = rcgChild
-                        }
-                        if(rcgChild.id == "select02") {
-                            select02 = rcgChild
-                        }
-                        if(rcgChild.id == "select03") {
-                            select03 = rcgChild
-                        }
-                        if(rcgChild.id == "select04") {
-                            select04 = rcgChild
-                        }
-                    });
-            
-                    var returnsCode_hidden = returnsCode;
-                    
-                    if(selectedOptionRCT == 00) {
-                        if(input00 != null) {
-                            input00.style.display='block';
-                            input00.value = returnsCode_hidden.value;
-                        }
+                function hiddeRestNodes(node1, node2, node3, node4, node5) {
+                    if (node1 != null) {
+                        node1.setAttribute("style","display:none;"); 
                     }
-                    else if(selectedOptionRCT == 01) {
-                        if(input01 != null) {
-                            input01.style.display='block';
-                            input01.value = returnsCode_hidden.value;
-                        }
+                    if (node2 != null) {
+                        node2.setAttribute("style","display:none;");
                     }
-                    else if(selectedOptionRCT == 02) {
-                        if(select02 != null) {
-                            select02.style.display='block';
-                            select02.value = returnsCode_hidden.value;
-                        }
+                    if (node3 != null) {
+                        node3.setAttribute("style","display:none;");
                     }
-                    else if(selectedOptionRCT == 03) {
-                        if(select03 != null) {
-                            select03.style.display='block';
-                            select03.value = returnsCode_hidden.value;
-                        }
-                    }
-                    else if(selectedOptionRCT == 04) {
-                        if(select04 != null) {
-                            select04.style.display='block'; 
-                            select04.value = returnsCode_hidden.value;
-                        }
-                    }
-                }
-
-                function hiddeNodes_whileInput(selectX, selectY, selectZ, inputX) {
-                    if (selectX != null) {
-                        selectX.setAttribute("style","display:none;");
-                    }
-                    if (selectY != null) {
-                        selectY.setAttribute("style","display:none;");
-                    }
-                    if (selectZ != null) {
-                        selectZ.setAttribute("style","display:none;");
-                    }
-                    if (inputX != null) {
-                        inputX.setAttribute("style","display:none;");
-                    }
-                }
-            
-                function hiddeNodes_whileSelect(selectX, selectY, inputX, inputY) {
-                    if (inputX != null) {
-                        inputX.setAttribute("style","display:none;"); 
-                    }
-                    if (inputY != null) {
-                        inputY.setAttribute("style","display:none;");
-                    }
-                    if (selectX != null) {
-                        selectX.setAttribute("style","display:none;");
-                    }
-                    if (selectY != null) {
-                        selectY.setAttribute("style","display:none;");
-                    }
+                    if (node4 != null) {
+                        node4.setAttribute("style","display:none;");
+                    } 
+                    node5.style.display='block';
                 }
             }
         });
